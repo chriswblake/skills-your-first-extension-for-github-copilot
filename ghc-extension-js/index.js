bf28a32bfacb072b5f6dce912133480cf6681064
Christopher W. Blake <chriswblake@github.com>
2025-02-22 02:55:00 +0000

    feat: add extension customizations

--- ghc-extension-js/index.js
+++ ghc-extension-js/index.js
@@ -35,48 +35,51 @@ app.post("/", express.json(), async (req, res) => {
   const messages = payload.messages;
 
   // Add the agent job description to copilot's messages
-  // const jobDescription = await fs.readFile(
-  //   path.join(__dirname, "agent-knowledge", "job-description.md"),
-  //   "utf8"
-  // );
-  // messages.unshift({
-  //   role: "system",
-  //   content: jobDescription,
-  // });
+  const jobDescription = await fs.readFile(
+    path.join(__dirname, "agent-knowledge", "job-description.md"),
+    "utf8"
+  );
+  messages.unshift({
+    role: "system",
+    content: jobDescription,
+  });
 
   // Add the school overview to copilot's messages
-  // const schoolOverview = await fs.readFile(
-  //   path.join(__dirname, "agent-knowledge", "school-overview.md"),
-  //   "utf8"
-  // );
-  // messages.unshift({
-  //   role: "system",
-  //   content: schoolOverview,
-  // });
+  const schoolOverview = await fs.readFile(
+    path.join(__dirname, "agent-knowledge", "school-overview.md"),
+    "utf8"
+  );
+  messages.unshift({
+    role: "system",
+    content: schoolOverview,
+  });
 
   // Add the staff descriptions to copilot's messages
-  // const staffDescriptions = await fs.readFile(
-  //   path.join(__dirname, "agent-knowledge", "staff-roles.md"),
-  //   "utf8"
-  // );
-  // messages.unshift({
-  //   role: "system",
-  //   content: staffDescriptions,
-  // });
+  const staffDescriptions = await fs.readFile(
+    path.join(__dirname, "agent-knowledge", "staff-roles.md"),
+    "utf8"
+  );
+  messages.unshift({
+    role: "system",
+    content: staffDescriptions,
+  });
 
   // Send messages array to copilot and collect the response
   const userToken = req.get("X-GitHub-Token");
-  const copilotResponse = await fetch("https://api.githubcopilot.com/chat/completions", {
-    method: "POST",
-    headers: {
-      authorization: `Bearer ${userToken}`,
-      "content-type": "application/json",
-    },
-    body: JSON.stringify({
-      messages,
-      stream: true,
-    }),
-  });
+  const copilotResponse = await fetch(
+    "https://api.githubcopilot.com/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${userToken}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        messages,
+        stream: true,
+      }),
+    }
+  );
 
   // Forward the response stream back to the user
   Readable.from(copilotResponse.body).pipe(res);
